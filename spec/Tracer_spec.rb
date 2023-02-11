be999ed7a15705cf8929de92a9481dbeadd31f6c
Sam ODonnell <samboy29@gmail.com>
2023-02-11 13:41:37 +1000

    Removes test from previous version Adds mostly useless tests for reworked version.

--- spec/Tracer_spec.rb
+++ spec/Tracer_spec.rb
@@ -2,75 +2,46 @@
 
 RSpec.describe Bugzilla do
 
+  # TODO: Write a proper test suite
+  describe "Tracer" do
+    before do
+      @tracer = Tracer.perform! do
+        puts "Hello world"
+      end
+    end
+
+    it "should have a version number" do
+      expect(Bugzilla::VERSION).not_to be nil
+    end
+
+    it "should have an array of traces" do
+      expect(@tracer.traces).to be_a(Array)
+    end
+
+    it "should have a count of ignored traces" do
+      expect(@tracer.ignored_traces_count).to be_a(Integer)
+    end
+  end
+
+  describe "Tracer::Trace" do
+
+  end
+
   describe "Tracer::Config" do
     before do
-      @tracer = Tracer.new { puts "Hello World" }
+      @tracer = Tracer.new
     end
 
-
-    it "has a max_stack_depth" do
-      expect(@tracer.config.max_stack_depth).to eq(100)
+    it "should have a config" do
+      expect(@tracer.config).to be_a(Tracer::Config)
     end
 
-    it "test" do
-      res = fake_trace!
-      count = res.executions.last.events.count
-
-      #expect count > 20
-      expect(count).to be > 20
+    it "should have a backtrace cleaner" do
+      expect(@tracer.backtrace_cleaner).to be_a(ActiveSupport::BacktraceCleaner)
     end
 
-    it "Should properly log events" do
-      results = trace do
-        sim = Simulation.new(dogs: 12, cats: 4, birds: 2)
-        sim.prepare
-      end
-
-      expect(results.events.count).to be > 100
+    it "should have a colourize boolean" do
+      expect(@tracer.colorize?).to be(true).or be(false)
     end
-
-    describe "It should be possible get SearchResults on the response" do
-      before do
-        @results = trace! do
-          sim = Simulation.new(dogs: 12, cats: 4, birds: 2)
-          sim.prepare
-        end
-      end
-
-      it "SearchResults: Returns SearchResults when querying trace results" do
-        output = @results.with_ivar("@start_config")
-
-        expect(output.class).to eq(SearchResults)
-      end
-
-
-      it "Can retrieve a tally of events" do
-        output = @results.tally_by(:event)
-
-        # get memory address of the output
-        output.object_id
-
-        expect(output[:call]).to be > 20
-        expect(output[:return]).to be > 20
-      end
-
-      it "Can retrieve a tally of defined_class" do
-        output = @results.tally_by(:defined_class)
-
-        expect(output[Simulation]).to be > 3
-        expect(output[Array]).to be > 3
-        expect(output[Cat]).to be > 3
-        expect(output[Animal]).to be > 3
-      end
-
-      it "Can retrieve results with matching instance var" do
-        original_output = @results.events.count
-        output = @results.with_ivar("@start_config")
-
-        expect(output.count).to be > 3
-        expect(output.count).to be < original_output
-      end
-    end
-
   end
 end
